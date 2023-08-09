2c7a12458a40b8b1e46571a093b00e8c09714257
Marcus Vasconcelos <marcus.cunha@ntendencia.com.br>
2023-08-09 16:28:53 -0300

    :sparkles: Added graph class

---
+++ classes/utils/Graph.js
@@ -0,0 +1,20 @@
+import CellularAutomata from "./CellularAutomata.js";
+
+export default class Graph {
+    constructor() {
+        this.nodes = [],
+        this.edges = []
+    }
+}
+
+class Edge {
+    constructor(origin, end) {
+        this.origin = origin;
+        this.end = end
+    }
+}
+class Node {
+    constructor(room) {
+        this.room = room
+    }
+}
